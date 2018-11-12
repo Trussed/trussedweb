21a2a16d7cdf0d217a7947eafb46951193cb3a52
Trussed <35123649+Trussed@users.noreply.github.com>
2018-11-11 23:36:25 -0800

    echo commit?

---
+++ resources/js/components/Home.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        This Is Content
+        <textarea></textarea>
+        <input type="text" />
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted(){
+            console.log("test");
+        }
+    }
+</script>
